aabf8f9d40f90208fdd41a360662405b1e72ac95
Bogdan <bogdan.lysenko.k@gmail.com>
2020-01-20 09:17:56 +0200

    fix-11

--- js/task.js
+++ js/task.js
@@ -81,18 +81,15 @@ console.log(getNamesSortedByFriendsCount(users));
 
 // task-10
 
-const getSortedUniqueSkills = users => {
-    return users
-        .reduce((allSkills, user) => {
-            user.skills.forEach(skill => {
-                if (!allSkills.includes(skill)) {
-                    allSkills.push(...user.skills);
-                }
-            });
-            return allSkills;
-        }, [])
-        .sort();
-};
+const getSortedUniqueSkills = users =>
+    users.reduce((allSkills, user) => {
+        user.skills.forEach(skill => {
+            if(!allSkills.includes(skill)){
+                allSkills.push(skill);
+            }
+        });
+        return allSkills.sort();
+    }, []);
 
 console.log(getSortedUniqueSkills(users));
 // [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure', 'laborum', 'lorem', 'mollit', 'non', 'nostrud', 'nulla', 'proident', 'tempor', 'velit', 'veniam' ]
